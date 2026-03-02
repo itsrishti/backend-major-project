1e48ad973d8cb26166614d623836aa5929667820
Piyush-singh-19 <piyush.vns19@gmail.com>
2026-03-02 19:45:01 +0530

    new changes

--- src/main/java/com/example/demo/Repository/BookingRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.Repository;
-
-
-
-import com.example.demo.Model.Booking;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface BookingRepository extends MongoRepository<Booking, String> {
-}
--- src/main/java/com/example/demo/Repository/TravelerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.Repository;
-
-
-
-import com.example.demo.Model.Traveler;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface TravelerRepository extends MongoRepository<Traveler, String> {
-}
