9ee2fa8de4921c5322d0bd9554f87fe1f55990cf
Piyush-singh-19 <piyush.vns19@gmail.com>
2026-03-03 16:08:56 +0530

    dataset

--- src/main/java/com/example/demo/Model/Museum.java
+++ src/main/java/com/example/demo/Model/Museum.java
@@ -4,7 +4,7 @@ import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document(collection = "Museumdata")
+@Document(collection = "dataset")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
